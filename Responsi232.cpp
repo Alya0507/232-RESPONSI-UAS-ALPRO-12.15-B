df105adb2b5663c9ba6abfc6a69ad1a4bab00f87
Alya0507 <alyazhafiraniha@gmail.com>
2025-07-10 13:36:37 +0700

    Add files via upload

---
+++ Responsi232.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class InputData;
+public(){
+	int nim;
+	int nama;	
+private:
+	string NilaiUTS;
+	string NilaiUAS;
+}
+void NilaiUts {
+	if (i = 0; i>1; i++){
+		if(j = 0; j>1; j++){
+			
+		}
+	}
+}
+
+int main(){
+	int nim,nama,UTS,UAS;
+	
+	cout<< ""
+}
